4c2c29edf6e72fe2f6b0240550992a78c55f91db
hony <1606302210@qq.com>
2024-01-09 12:00:04 +0800

    first

--- mall-master/include/ueditor_upload_path.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//ueditor编辑器文件上传路径前缀,后缀在 lib/ueditor/php目录里配置
-
-define('UPLOADROOT',str_replace($_SERVER['DOCUMENT_ROOT'],'',str_replace('\\','/',dirname(dirname(__FILE__))).'/'));
-//print_r(UPLOADROOT);
-
-?>
